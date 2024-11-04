7ce900a9233dd1c1d75390074b8a45bf40f53e38
shineamond <minh2005nd@gmail.com>
2024-11-04 22:11:48 +0700

    Enabled removing sensitive info from PDF. Enabled creating chunks from text.

--- App.py
+++ App.py
@@ -1,14 +1,38 @@
 import pdfplumber
 import logging
+import re
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
 def extract_text_from_pdf(pdf_path):
+    sensitive_info_patterns = [
+        r'\b\d{9,12}\b',
+        r'\b\d{10}\b',
+        r'\b[\w\.-]+@[\w\.-]+\.\w+\b'
+    ]
+    sensitive_info_patterns = [re.compile(pattern) for pattern in sensitive_info_patterns]
+
+    bullet_patterns = [
+        r'^\s*[\u2022\u25E6\u26AB]\s+',
+        r'^\s*[-•∙◦◎⦿⦾]\s+'
+    ]
+    bullet_patterns = [re.compile(pattern, re.MULTILINE) for pattern in bullet_patterns]
+
     text = ""
     try:
         with pdfplumber.open(pdf_path) as pdf:
             for page in pdf.pages:
                 try:
-                    text += page.extract_text() or ""
+                    filtered_text = page.extract_text() or ""
+
+                    for pattern in sensitive_info_patterns:
+                        filtered_text = pattern.sub('', filtered_text)
+
+                    for pattern in bullet_patterns:
+                        filtered_text = pattern.sub('', filtered_text)
+
+                    text += filtered_text
+
                 except Exception as e:
                     logging.warning(f"Không thể đọc trang PDF: {str(e)}")
                     continue
@@ -21,9 +45,16 @@ def extract_text_from_pdf(pdf_path):
     return text
 
 
+def create_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 200, length_function = len)
+    chunks = text_splitter.split_text(text)
+
+    return chunks
+
+
 def main():
     text = extract_text_from_pdf(".\\data\\04.pdf")
-    print(text)
-
+    chunks = create_chunks(text)
+    print(chunks)
 
 main()
